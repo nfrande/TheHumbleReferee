af16d9c5f03bc40f4025a9ccc0273a3e20217d22
KickTheBoot <anoojjonttu@gmail.com>
2023-05-16 14:43:55 +0300

    Create CharacterDestination script that imitates AIDestinationSetter's behavior

---
+++ Assets/Scripts/CharacterDestination.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class CharacterDestination : VersionedMonoBehaviour
+{
+    [SerializeField] float distance;
+    [SerializeField] BallBehaviour ball;
+    [SerializeField] Vector2 GoalPosition;
+
+    IAstarAI ai;
+
+    [SerializeField] Team team;
+
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        switch (team)
+        {
+            case Team.team1:
+                GoalPosition = new Vector2(5, 0);
+                break;
+            case Team.team2:
+                GoalPosition = new Vector2(-5, 0);
+                break;
+        }
+        ai = GetComponent<IAstarAI>();
+        if (ai != null) ai.onSearchPath += Update;
+    }
+
+    void OnDisable()
+    {
+        if (ai != null) ai.onSearchPath -= Update;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ai.destination = CalculateDestination();
+        Debug.DrawLine(ball.transform.position, CalculateDestination(),Color.magenta,0.1f);
+    }
+
+    Vector2 CalculateDestination()
+    {
+        return (Vector2)ball.transform.position + ((Vector2)ball.transform.position - GoalPosition).normalized * distance;
+    }
+
+}
+
+public enum Team
+{
+    team1,
+    team2
+}
---
+++ Assets/Scripts/FieldInfo.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEditor.ShortcutManagement;
+
+public class FieldInfo : MonoBehaviour
+{
+    public static FieldInfo instance;
+    public Vector2 fieldSize;
+
+    public Vector2 team1Goal, team2Goal;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position, fieldSize);
+        
+        Gizmos.DrawCube(team1Goal, Vector3.one*0.25f);
+        Gizmos.DrawCube(team2Goal, Vector3.one*0.25f);
+    }
+
+    void OnToolGUI(EditorWindow window)
+    {
+        fieldSize = Handles.PositionHandle(fieldSize, Quaternion.Euler(0,0,0));
+    }
+}
+
+[EditorTool("Soccer field tool", typeof(FieldInfo))]
+public class FieldTool : EditorTool, IDrawSelectedHandles
+{
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+    public void OnDrawHandles()
+    {
+        FieldInfo[] infos = Selection.GetFiltered<FieldInfo>(SelectionMode.TopLevel);
+        if(infos.Length > 0)
+        {
+            infos[0].fieldSize = Handles.PositionHandle(infos[0].fieldSize, Quaternion.Euler(0,0,0));
+        }
+    }
+
+    [Shortcut("Enable soccer field tool",typeof(SceneView),KeyCode.B)]
+    public void Activate()
+    {
+        if(Selection.GetFiltered<FieldInfo>(SelectionMode.TopLevel).Length > 0)
+        Debug.Log("FieldTool");
+    }
+
+    public override void OnToolGUI(EditorWindow window)
+    {
+        Handles.BeginGUI();
+        Handles.EndGUI();
+    }
+}
